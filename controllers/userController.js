b849a3515e02ebbcc722bd61faf3259830d72313
Ron Crawford <roncrawford1300@gmail.com>
2022-07-31 18:42:25 -0400

    Updating naming

---
+++ controllers/userController.js
@@ -0,0 +1,108 @@
+const { ObjectId } = require('mongoose').Types;
+const { User, Thought } = require('../models');
+
+// Aggregate function to get the number of users overall
+const headCount = async () =>
+  User.aggregate()
+    .count('userCount')
+    .then((numberOfUsers) => numberOfUsers);
+
+module.exports = {
+  // Get all user
+  getUsers(req, res) {
+    Student.find()
+      .then(async (users) => {
+        const userObj = {
+          users,
+          headCount: await headCount(),
+        };
+        return res.json(userObj);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  // Get a single user
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select('-__v')
+      .then(async (user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json({
+            user,
+              thought: await thought(req.params.userId),
+            })
+      )
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  // create a new user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Delete a user 
+  deleteUser(req, res) {
+    User.findOneAndRemove({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No such user exists' })
+          : Thought.findOneAndUpdate(
+              { user: req.params.userId },
+              { $pull: { studenuserts: req.params.userId } },
+              { new: true }
+            )
+      )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({
+              message: 'User deleted, but no thoughts found',
+            })
+          : res.json({ message: 'Thought successfully deleted' })
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  // Add a thought to a user
+  addThought(req, res) {
+    console.log('You are adding a thought');
+    console.log(req.body);
+    Student.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { thoughts: req.body } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res
+              .status(404)
+              .json({ message: 'No user found with that ID :(' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // Remove thought from a user
+  removeThought(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { thought: { thoughtId: req.params.thoughtId } } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res
+              .status(404)
+              .json({ message: 'No user found with that ID :(' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+};
